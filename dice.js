71ac9675b250c473846c49cb122429568510b4f2
jashfield730 <jashfield730@gmail.com>
2013-12-16 19:50:08 +0000

    Uploading New Files

---
+++ dice.js
@@ -0,0 +1,29 @@
+﻿function diceroller()
+{
+var times = [];
+times[0] = document.getElementById('comp1').value;
+times[1] = document.getElementById('comp2').value;
+times[2] = document.getElementById('comp3').value;
+var rolls = document.getElementById('rolls').value;
+//for (var i=0;i<number.length;i++)
+//{ 
+//    times[i] =  comparison1
+//}
+while (times.length > 0) {
+    var n = 0;
+    var checkpoint = times[n];
+    var amount = 0;
+    while (rolls > 0) {
+        var dice = Math.floor(Math.random() * 6) + 1;
+        if (dice >= checkpoint) {
+            amount = amount + 1
+        }
+        rolls = rolls - 1;
+    }
+    times.shift();
+    rolls = amount;
+    amount = 0;
+   // console.log(rolls);
+    document.getElementById("output").value = rolls;
+}
+}
