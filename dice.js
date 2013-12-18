91609945f1c3ea203d5cb77c0b5290d16e84f9ff
jashfield730 <jashfield730@gmail.com>
2013-12-18 16:33:01 +0000

    Added Functionality of the Special Function

--- dice.js
+++ dice.js
@@ -3,12 +3,18 @@
 var times = [];
 times[0] = document.getElementById('comp1').value;
 times[1] = document.getElementById('comp2').value;
-times[2] = document.getElementById('comp3').value;
+if (special1.checked == 1){
+}
+else
+{
+	times[2] = document.getElementById('comp3').value;
+}
 var rolls = document.getElementById('rolls').value;
 //for (var i=0;i<number.length;i++)
 //{ 
 //    times[i] =  comparison1
 //}
+
 while (times.length > 0) {
     var n = 0;
     var checkpoint = times[n];
